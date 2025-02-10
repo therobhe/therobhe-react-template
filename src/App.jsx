bb0b89d0596f4315cd4c2f11ff7d560a4db27908
robhe <rheinemann97@gmail.com>
2025-02-10 20:39:38 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/areas/Header.jsx";
+import Main from "./components/areas/Main.jsx";
+import Footer from "./components/areas/Footer.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Header></Header>
+            <Main></Main>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App
